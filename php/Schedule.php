40966597ae1dcf3e8730450558b0c519f88e2aa1
George <dantg21@hanover.edu>
2021-02-15 00:20:19 -0500

    add scripting for if the query works

--- php/Schedule.php
+++ php/Schedule.php
@@ -6,5 +6,12 @@
     if (isset($_POST['submit'])) {  
         for ($i=0; $i<sizeof ($checkbox1);$i++) {  
         $query = mysqli_query($conn, "INSERT INTO players(chk_boxes) VALUES ('".$checkboxl[$i]."') WHERE username ='" . $_POST["userName"] . "'");
+        }
+        if($query == TRUE){
+            echo "<script> location.assign('../html/Player_schedule_creator.html'); </script>";
+        }
+        else {
+            echo "<script> location.assign('../html/Player_schedule_creator.html'); </script>";
+        }
     } 
 ?> 
